099923a2e6f0b0826235979076d9927387b96f5a
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-12 22:26:07 +0900

    react

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // App 컴포넌트를 불러옴
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
