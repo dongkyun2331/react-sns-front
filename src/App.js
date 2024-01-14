c89d49547476ba6a6f11bca6f5e2e661165cdae9
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-14 23:16:57 +0900

    react-firebase-hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Chats from "./components/Chats";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/chats" element={<Chats />} />
       </Routes>
     </Router>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GoogleOutlined } from "@ant-design/icons";
 import "firebase/compat/app";
 
 import { auth } from "../firebase";
 import firebase from "firebase/compat/app";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    {
+      if (user) {
+        {
+          navigate("/chats");
+        }
+      }
+    }
+  }, [user, navigate]);
+
   return (
     <div id="login-page">
       <div id="login-card">
