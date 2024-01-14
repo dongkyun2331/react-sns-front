12f1f7a9bb7f1fe006b18357676377eee6b35973
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-14 19:49:38 +0900

    @ant-design/icons

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "firebase/compat/app";
+
+import { auth } from "../firebase";
+import firebase from "firebase/compat/app";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div id="login-page">
+      <div id="login-card">
+        <div
+          className="login-button google"
+          onClick={() =>
+            auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+          }
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
