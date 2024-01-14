338fd3f8f2a5465f3a29a52023c15bf3d559811a
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-14 23:34:29 +0900

    logout

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+
+    navigate("/");
+  };
+
+  return (
+    <div>
+      {user && (
+        <div>
+          <img src={user.photoURL} alt={user.displayName} />
+          <p>{user.displayName}</p>
+          <button onClick={handleLogout} className="logout-tab">
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Chats;
