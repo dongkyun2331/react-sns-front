907df13ddaf7f7a380e9a9ee794a61a7e6866152
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-18 21:48:46 +0900

    scrollIntoView

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { db } from "../firebase";
 import "./PostList.css";
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
+  const messagesEndRef = useRef(null);
 
   useEffect(() => {
     const postsRef = db.ref("posts");
@@ -21,6 +22,14 @@ const PostList = () => {
     });
   }, []);
 
+  useEffect(() => {
+    scrollToBottom();
+  }, [posts]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <ul className="post-list">
       {posts.map((post) => (
@@ -37,6 +46,7 @@ const PostList = () => {
           </div>
         </li>
       ))}
+      <div ref={messagesEndRef}></div>
     </ul>
   );
 };
